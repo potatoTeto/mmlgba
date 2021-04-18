cad0dd448495937a25ca741578bbacf1b3c94229
potatoTeto <potato.cos@gmail.com>
2021-04-18 04:32:12 -0700

    Initial commit

---
+++ gbsound_testing/source/mmlgba/directsound.c
@@ -0,0 +1,11 @@
+#include "directsound.h"
+
+#include <gba_console.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_timers.h>
+#include <gba_input.h>
+
+void directSoundUpdate(void) {
+  // TODO: Migrate the DirectSound engine
+}
---
+++ gbsound_testing/source/parser/src/ParserException.java
@@ -0,0 +1,9 @@
+public class ParserException extends Exception {
+	public ParserException(String message) {
+		super(message);
+	}
+
+	public ParserException(String message, Lexer.Token token) {
+		super(String.format("On line %d column %d: %s", token.line, token.pos, message));
+	}
+}
